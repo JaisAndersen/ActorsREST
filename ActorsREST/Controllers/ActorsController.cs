a44358d69b7e2e576779a13a443bf38e8418f27c
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-14 11:16:24 +0100

    Cors added

--- ActorsREST/Controllers/ActorsController.cs
+++ ActorsREST/Controllers/ActorsController.cs
@@ -2,6 +2,7 @@
 using ActorRepositoryLibrary;
 using ActorsREST.Records;
 using System.Data.Common;
+using Microsoft.AspNetCore.Cors;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
--- ActorsREST/Program.cs
+++ ActorsREST/Program.cs
@@ -3,6 +3,16 @@ using ActorRepositoryLibrary;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+        policy =>
+        {
+            policy.WithOrigins("https://google.dk");
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+        });
+});
 
 builder.Services.AddControllers();
 
@@ -11,6 +21,7 @@ builder.Services.AddSingleton<ActorsRepositoryList>(new ActorsRepositoryList());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
